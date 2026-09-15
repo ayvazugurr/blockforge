5f746fb49e80df5cfe4ca14227cfcbd944dce884
me182group8b <ayvazs.ugurr@gmail.com>
2026-09-15 12:44:54 +0300

    fix: render Holo star-H signature reliably on iOS v1.0.7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE_NAME="blockforge-v1.0.6";
+const CACHE_NAME="blockforge-v1.0.7";
 const CORE_ASSETS=["./","./index.html","./style.css","./game.js","./manifest.webmanifest","./icons/icon.svg","./icons/icon-maskable.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS.map(url=>new Request(url,{cache:"reload"})))));
@@ -23,4 +23,5 @@ self.addEventListener("fetch",event=>{
 self.addEventListener("message",event=>{
   if(event.data?.type==="ACTIVATE_UPDATE") event.waitUntil(self.skipWaiting());
 });
+
 
